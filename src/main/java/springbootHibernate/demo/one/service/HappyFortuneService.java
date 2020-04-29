4923b96b2dfcac04e8c8e433626c58b184741358
Fabrice Da Silva <f-dasilva2@autodistribution.com>
2020-04-29 16:38:48 +0200

    Added: random pick up from array for FortuneService

--- src/main/java/springbootHibernate/demo/one/service/HappyFortuneService.java
+++ src/main/java/springbootHibernate/demo/one/service/HappyFortuneService.java
@@ -2,8 +2,15 @@ package springbootHibernate.demo.one.service;
 
 public class HappyFortuneService implements FortuneService {
 
+    private String[] array = {
+            "It is your lucky day",
+            "It is not your lucky day",
+            "It is your normal average day"
+    };
+
     @Override
     public String getFortune() {
-        return "It is your lucky day";
+
+        return array[(int)(System.currentTimeMillis() % array.length)];
     }
 }
